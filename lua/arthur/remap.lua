c2fd80c24f0c56129d6867fbeebf2114bcbc1097
arthur-carvalho <arthur.teixeira@objective.com.br>
2023-01-16 10:07:51 -0300

    Change netrw remap

--- lua/arthur/remap.lua
+++ lua/arthur/remap.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>zx", vim.cmd.Ex)
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
