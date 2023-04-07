ac1500ceb3d5cb5b1b8fc4069fc0436fce211de6
arthur-teixeira <arthurcarvalhot@yahoo.com.br>
2023-04-07 18:02:44 -0300

    Shows gitignore files on tree

--- after/plugin/filetree.lua
+++ after/plugin/filetree.lua
@@ -1,4 +1,17 @@
-require('nvim-tree').setup()
+require('nvim-tree').setup({
+  sort_by = "case_sensitive",
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = false,
+  },
+  git = {
+    enable = true,
+    ignore = false,
+    timeout = 500,
+  },
+})
 
 vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
 vim.keymap.set('n', '<leader>ff', vim.cmd.NvimTreeFindFile)
