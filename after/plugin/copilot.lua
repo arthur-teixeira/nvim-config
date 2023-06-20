3dbf0564a4af15dca71cb2406497099a0f7fda86
arthur-carvalho <arthur.teixeira@objective.com.br>
2023-06-20 11:04:19 -0300

    Changed copilot accept key to Control + s

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -7,7 +7,7 @@ require("copilot").setup({
     auto_trigger = true,
     debounce = 75,
     keymap = {
-      accept = "<leader>c",
+      accept = "<C-s>",
       accept_word = false,
       accept_line = false,
       next = "<M-]>",
